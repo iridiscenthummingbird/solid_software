52a2f1a610b37da928146b6cea55c11f1def09aa
iridiscent-hummingbird <0508dima@gmail.com>
2021-08-31 23:01:55 +0300

    Add color changing

--- lib/ColorProvider.dart
+++ lib/ColorProvider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class ColorProvider extends ChangeNotifier {
   Color _color = Color.fromARGB(255, 255, 255, 255);
-
   Color get color => _color;
 
+  bool _isWhiteText = false;
+  bool get isWhiteText => _isWhiteText;
+
   void changeColor() {
-    print("change");
+    var rng = new Random();
+    int r = rng.nextInt(256);
+    int g = rng.nextInt(256);
+    int b = rng.nextInt(256);
+    _color = Color.fromARGB(255, r, g, b);
+
+    //if color is too dark => change text color to white
+    _isWhiteText = (r + g + b) <= 255 ? true : false;
+
+    notifyListeners();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,29 +9,36 @@ void main() {
       child: MyApp(),
     ),
   );
-  //runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Solid Software Test Task',
-      home: Home(),
-    );
+        title: 'Solid Software Test Task',
+        home: HomeScreen(),
+        debugShowCheckedModeBanner: false);
   }
 }
 
-class Home extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: context.read<ColorProvider>().color,
+        backgroundColor: context.watch<ColorProvider>().color,
         body: GestureDetector(
           behavior: HitTestBehavior.opaque,
           onTap: () => context.read<ColorProvider>().changeColor(),
           child: Center(
-            child: Text("Text"),
+            child: Text(
+              "Hey there",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: context.watch<ColorProvider>().isWhiteText
+                      ? Colors.white
+                      : Colors.black),
+            ),
           ),
         ));
   }
